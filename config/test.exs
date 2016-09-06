1ebd9d72af406c5758dcdcfe6dd811ed68c98f13
Chris Dutton <chris@rosepointnav.com>
2016-09-06 15:52:06 -0700

    Add basic testing

--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,14 @@
 use Mix.Config
+
+config :nerves_leds,
+         names: [ test_led: &LoggerLedBackendTest.on_led_write/1 ],
+         states: [
+           true: [brightness: 1],
+           false: [brightness: 0],
+           slowblink: [trigger: "timer", delay_off: 250, delay_on: 250],
+           fastblink: [trigger: "timer", delay_off: 80, delay_on: 50],
+           slowwink: [trigger: "timer", delay_on: 1000, delay_off: 100],
+           heartbeat: [trigger: "heartbeat"],
+           test_state: [foo: 3, bar: 29, baz: %{}]]
+
+config :logger_led_backend, led: :test_led
--- test/logger_led_backend_test.exs
+++ test/logger_led_backend_test.exs
@@ -1,8 +1,57 @@
 defmodule LoggerLedBackendTest do
   use ExUnit.Case
-  doctest LoggerLedBackend
+  require Logger
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  #IMPORTANT: These tests should be improved. Sometimes they will fail due to
+  #how logger events are triggered/processed. There is a sleep in each test
+  #to help with this, but is not fool proof.
+
+  Logger.add_backend LoggerLedBackend
+
+  setup_all do
+    :ets.new :logger_test, [:named_table, :set, :public]
+    :ok
+  end
+
+  # callback for the test led that simply sets keys in and ets table
+  def on_led_write({k, v}) do
+    :ets.insert :logger_test, [{k, v}]
+  end
+
+  test "valid event - error" do
+    Logger.error "Test"
+    sleep()
+    assert check(:brightness) == 1
+    :ets.insert :logger_test, brightness: 0
+  end
+
+  test "invalid event - debug" do
+    Logger.debug "Debug Test"
+    sleep()
+    assert check(:brightness) == 0
+    :ets.insert :logger_test, brightness: 0
+  end
+
+  test "invalid then valid - debug" do
+    Logger.debug "Debug Test"
+    sleep()
+    assert check(:brightness) == 0
+    Logger.configure_backend LoggerLedBackend, [level: :debug]
+    :timer.sleep(500)
+    Logger.debug "Debug Test"
+    sleep()
+    assert check(:brightness) == 1
+    :ets.insert :logger_test, brightness: 0
+  end
+
+  defp check(key) do
+    case :ets.lookup(:logger_test, key) do
+      [] -> nil
+      [{_, v}] -> v
+    end
+  end
+
+  defp sleep() do
+    :timer.sleep 15 #sleep to let Logger process event
   end
 end
